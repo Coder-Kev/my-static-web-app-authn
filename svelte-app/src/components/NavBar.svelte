7778b3e1b0b7c41f959259774468d0a9a82768ed
Kevin Terlesky <Coder-Kev@users.noreply.github.com>
2021-09-28 13:29:33 -0700

    Add authentication

--- svelte-app/src/components/NavBar.svelte
+++ svelte-app/src/components/NavBar.svelte
@@ -1,6 +1,9 @@
 <script>
   import { Link } from 'svelte-routing';
 
+  const providers = ['twitter', 'github', 'aad'];
+  const redirect = window.location.pathname;
+
   function getProps({ href, isPartiallyCurrent, isCurrent }) {
     const isActive = href === '/' ? isCurrent : isPartiallyCurrent || isCurrent;
 
@@ -10,6 +13,24 @@
     }
     return {};
   }
+
+  import { onMount } from 'svelte';
+
+let userInfo = undefined;
+
+onMount(async () => (userInfo = await getUserInfo()));
+
+async function getUserInfo() {
+  try {
+    const response = await fetch('/.auth/me');
+    const payload = await response.json();
+    const { clientPrincipal } = payload;
+    return clientPrincipal;
+  } catch (error) {
+    console.error('No profile could be found');
+    return undefined;
+  }
+}
 </script>
 
 <div class="column is-2">
@@ -20,4 +41,32 @@
       <Link to="/about" {getProps}>About</Link>
     </ul>
   </nav>
+  <nav class="menu auth">
+    <p class="menu-label">Auth</p>
+    <div class="menu-list auth">
+      {#if !userInfo}
+        {#each providers as provider (provider)}
+          <a href={`/.auth/login/${provider}?post_login_redirect_uri=${redirect}`}>
+            {provider}
+          </a>
+        {/each}
+      {/if}
+      {#if userInfo}
+        <a href={`/.auth/logout?post_logout_redirect_uri=${redirect}`}>
+          Logout
+        </a>
+      {/if}
+    </div>
+  </nav>
+  {#if userInfo}
+  <div class="user">
+    <p>Welcome</p>
+    <p>{userInfo && userInfo.userDetails}</p>
+    <p>{userInfo && userInfo.identityProvider}</p>
+  </div>
+  {:else}
+  <div class="user">
+    You are not logged in
+  </div>
+  {/if}
 </div>
